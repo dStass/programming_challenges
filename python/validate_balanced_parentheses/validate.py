b44d0d5924d8a8fbd44e0ec61d442403eb41b33f
dStass <dstiasny@gmail.com>
2019-08-23 18:21:34 +1000

    validate.py clean up

--- python/validate_balanced_parentheses/validate.py
+++ python/validate_balanced_parentheses/validate.py
@@ -2,9 +2,13 @@
 From: Daily Interview Pro 20/08/2019
 Problem: Validate Balanced Parentheses
 
-Imagine you are building a compiler. Before running any code, the compiler must check that the parentheses in the program are balanced. Every opening bracket must have a corresponding closing bracket. We can approximate this using strings.
+Imagine you are building a compiler.
+Before running any code, the compiler must check that the parentheses
+in the program are balanced. Every opening bracket must have a corresponding
+closing bracket. We can approximate this using strings.
 
-Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid.
 An input string is valid if:
 - Open brackets are closed by the same type of brackets.
 - Open brackets are closed in the correct order.
@@ -15,14 +19,14 @@ pairs = {'(': ')', '[': ']', '{': '}'} # each open char keys to a close
 
 class Solution:
   def isValid(self, s):
-    stack = [] # list
+    stack = [] # list of chars -> str
     for c in s:
       # if c is an opening char, add it to stack
       if c in pairs:
         stack += c
       else: # if c is a closing char
         if c == pairs.get(stack[-1]): # check if it matches last thing in the stack
-          stack = stack[:-1]
+          stack = stack[:-1] # pop the stack
         else:
           return False
 
@@ -30,8 +34,6 @@ class Solution:
       return False
 
     return True
-
-    # Fill this in.
 
 # Test Program
 s = "()(){(())"

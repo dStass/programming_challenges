c17976021474d24549a478a6e0ca26055444002a
dStass <dstiasny@gmail.com>
2019-08-24 14:32:10 +1000

    longest non-repeating substring fix
    instead of looking at the strings themselves, we now coonsider lengths only

--- python/longest_nonrepeating_substring/lnrsub.py
+++ python/longest_nonrepeating_substring/lnrsub.py
@@ -10,18 +10,32 @@ class Solution:
 
   # Solution will return first occurence of max length substring
   def lengthOfLongestSubstring(self, s):
-    longest = ""  # longest found non-repeating substring
-    so_far = ""  # non-repeating substring so far
+    longest = 0  # max len
+    so_far = 0  # non-repeating len so far
+    last_visited = {}  # stores location of last visited
+    index = 0
+
+    last_visited[s[0]] = 0
     for c in s:
-      if c not in so_far:  # if c is a non-repeating character
-        so_far += c  # add it to current string
-        if len(so_far) > len(longest):  # if current substring is longer than previous longest
-          longest = so_far  # set longest to so_far
+      if index == 0:
+        index += 1
+        continue
+
+      index += 1
+      prev_c = last_visited.get(c)
+      if prev_c == None or index - so_far > prev_c:
+        so_far += 1
       else:
-        so_far = "" + c  # if c IS repeated, start current sub_string again with JUST that char
-    return len(longest)
+        if so_far > longest:
+          longest = so_far
+        so_far = index - prev_c
+      last_visited[c] = index
+
+    # final check for so_far reaching end of string
+    if so_far > longest:
+      longest = so_far
+    return longest
+
 
 print(Solution().lengthOfLongestSubstring('abrkaabcdefghijjxxx'))
-print(Solution().lengthOfLongestSubstring('abcdefghaijklfghijklmnooo'))
-
-# 10
+print(Solution().lengthOfLongestSubstring('abcdefghijalmno'))

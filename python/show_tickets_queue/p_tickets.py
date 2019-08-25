c4b5eafed66645444beb75cf2edf664f33be3cc8
dStass <dstiasny@gmail.com>
2019-08-25 11:20:13 +1000

    p_tickets challenge completed

--- python/show_tickets_queue/p_tickets.py
+++ python/show_tickets_queue/p_tickets.py
@@ -13,7 +13,44 @@ The venue sells tickets according to the following rules:
 '''
 
 import sys
+import time
 
+
+# list of tickets [t0, t1, ..., tp, ... tN-1]
+# if i < p, add p if tix[i] > tix[p] else add tix[i]
+# if i > p and tix[i] < tix[p], we add tix[i] else add tix[p]-1
+# add tix[p]
+#
+# People in FRONT of p will either:
+#   1) Get all their tickets IF ticket count less than OR equal p's tikets
+#   2) if they have more tickets than p's, they will only get the same amount of
+#      tickets p gets before timer stops
+#
+# People behind p similarly will:
+#   1) get all their tickets if they want less tickets than p
+#   2) get the same amount of tickets as p MINUS 1 (as they are behind)
+#      and timer stops before they get their pth ticket
+#
+# Finally we add the amount of tickets p wants
+# Here tickets amounts to the same amount of time
+#
+def p_time(p, tickets):
+  t1 = time.time()
+  total_seconds = 0
+  for i in range(0, len(tickets)):
+    if i < p:
+      total_seconds += tickets[p] if tickets[i] > tickets[p] else tickets[i]
+    elif i == p:
+      total_seconds += tickets[p]
+    else:
+      total_seconds += tickets[p]-1 if tickets[i] > tickets[p] else tickets[i]
+  print("p_time time taken = ", (time.time() - t1))
+  return total_seconds
+
+
+
+
+# interesting result:
 # assume p < N
 # Logic: for list of N items from [0 . . . N-1]
 # When we pop the queue at any position k, add k-1 to the end of the list
@@ -39,12 +76,22 @@ import sys
 # As such for p'th object will traverse an extra p * (N-p-1) seconds
 # Total = sum of t(L1) and t(L2) = ((p+1)(p+2))/2 + p(N-p-1) seconds
 def p_time_natural_sorted(p, N):
+  t1 = time.time()
   if p < 0 or p >= N:
     return 0
   solution = 0.5*((p+2)*(p+1)) + (p)*(N-p-1)
+  # or solution = 0.5*((p)*(p+1)) + (p)*(N-p) + 1
+  print("p_time_natural_sorted time taken = ", time.time() - t1)
   return int(solution)
 
-p = int(sys.argv[1])
-N = int(sys.argv[2])
 
-print("time for p = ", p, ", N = ", N, ", t = ", p_time_natural_sorted(p, N), sep='')
+p = 1455
+N = 20000026
+tickets = [11,2,5,9,4]
+
+tickets_natural = list(range(1, N+1))
+
+
+# print("time for p = ", p, ", tix = ", tickets, ", t = ", p_time(p, tickets), sep='')
+print("time for p = ", p, ", tix = ", len(tickets_natural), ", t = ", p_time(p, tickets_natural), sep='')
+print("time for p = ", p, ", tix = ", N, ", t = ", p_time_natural_sorted(p, N), sep='')
